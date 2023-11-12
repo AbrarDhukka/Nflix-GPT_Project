be00418b870f18b0f82109fac8f6776d3a3ce1e8
AbrarDhukka <abrdhk@gmail.com>
2023-11-12 12:22:49 +0530

    updated

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,7 +1,11 @@
-import {configureStore} from "@reduxjs/toolkit"
+import {configureStore} from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
+
 const appStore = configureStore(
     {
-        reducer:{}
+        reducer:{
+            user:userReducer,
+        }
     }
 );
 
