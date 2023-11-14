d9ab5b742654d35ccc366c8498a0a3da9b09ddf6
AbrarDhukka <abrdhk@gmail.com>
2023-11-14 21:57:48 +0530

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,38 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
-  
+  const location = useLocation();
+
   return (
     <div className="w-full absolute bg-gradient-to from-black z-30">
       <div className="w-[80%]  mx-auto flex justify-between items-center flex-wrap bg-gradient-to from-black">
         <div className="w-36 sm:w-44">
-          <Link to="/"><img
-            src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-            alt="logo"
-          ></img></Link>
+          <Link to="/">
+            <img
+              src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+              alt="logo"
+            ></img>
+          </Link>
         </div>
         <div className="m-2 flex justify-between items-center flex-wrap sm:m-3">
-          <div className="m-1 sm:m-3 border p-1 rounded-md text-white">Language</div>
-         <Link to="/login"> <button className="m-1 sm:m-3 border p-1 px-3 rounded-md bg-red-600 text-white font-semibold">Sign In</button></Link>
+          <div className="m-1 sm:m-3 border p-1 rounded-md text-white">
+            Language
+          </div>
+
+          {location.pathname === "/" || "/login" ? (
+            <Link to="/login">
+              <button className="m-1 sm:m-3 border p-1 px-3 rounded-md bg-red-600 text-white font-semibold">
+                Sign In
+              </button>
+            </Link>
+          ) : (
+            <Link to="/">
+              <button className="m-1 sm:m-3 border p-1 px-3 rounded-md bg-red-600 text-white font-semibold">
+                Sign Out
+              </button>
+            </Link>
+          )}
         </div>
       </div>
     </div>
@@ -22,4 +40,3 @@ const Header = () => {
 };
 
 export default Header;
-
