9673433d9997ee9f098945656ef90f289b22eae6
AbrarDhukka <abrdhk@gmail.com>
2023-11-17 08:14:49 +0530

    updated

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZmM3Yzc4YWYxY2RlNzNlNWVlNzU1N2VhNzNmMTM4YSIsInN1YiI6IjY1NTZkMGQ5YjU0MDAyMTRkM2M5YjU4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZaRZxfgxXL0qhDgYP6RiHn16rCrwalE2HRTOlwUXUTk'
+    }
+  };
