679a71dad9dd3b6cd21440ff72cea14c24d407c4
AbrarDhukka <abrdhk@gmail.com>
2023-11-17 10:46:04 +0530

    added custom hooks

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,34 +1,15 @@
-import React, { useEffect } from 'react'
-import Header from './Header'
-import { API_OPTIONS } from '../utils/constants'
-import { useDispatch } from 'react-redux';
-import { addNowPlayingMovies } from '../utils/moviesSlice';
-
+import React from "react";
+import Header from "./Header";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 const Browse = () => {
-
-  const dispatch = useDispatch();
-
-  const getNowPlayingMovies = async ()=>{
-   const data=await fetch('https://api.themoviedb.org/3/movie/now_playing?&page=1', API_OPTIONS);
-   const json= await data.json();
-   console.log(json.results)
-   dispatch(addNowPlayingMovies(json.results))
-  }
-
-  useEffect(()=>{
-    getNowPlayingMovies();
-}, []);
-
+  useNowPlayingMovies();
   return (
     <div>
       <Header></Header>
-      <div className='bg-black w-screen h-20'>
-      </div>
-      <div>
-        Broswe
-      </div>
+      <div className="bg-black w-screen h-20"></div>
+      <div>Broswe</div>
     </div>
-  )
-}
+  );
+};
 
-export default Browse
+export default Browse;
