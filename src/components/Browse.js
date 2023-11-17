cec873fc750b2f065ec782883479d5b922271b28
AbrarDhukka <abrdhk@gmail.com>
2023-11-17 19:11:10 +0530

    developed browse page

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 const Browse = () => {
   useNowPlayingMovies();
   return (
     <div>
       <Header></Header>
-      <div className="bg-black w-screen h-20"></div>
-      <div>Broswe</div>
+      {/* <div className="bg-black w-screen h-20"></div> */}
+      <div>
+      <MainContainer />
+          <SecondaryContainer />
+      </div>
     </div>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,5 @@ export const API_OPTIONS = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZmM3Yzc4YWYxY2RlNzNlNWVlNzU1N2VhNzNmMTM4YSIsInN1YiI6IjY1NTZkMGQ5YjU0MDAyMTRkM2M5YjU4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZaRZxfgxXL0qhDgYP6RiHn16rCrwalE2HRTOlwUXUTk'
     }
   };
+
+  export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
