4a7a765111e25a79593b57d7ceeed1ba319cda2b
AbrarDhukka <abrdhk@gmail.com>
2023-11-23 22:51:21 +0530

    Responsive added

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -4,11 +4,15 @@ import { useDispatch, useSelector } from "react-redux";
 import lang from "../utils/languageConstants";
 import { API_OPTIONS } from "../utils/constants";
 import { addGptMovieResult } from "../utils/gptSlice";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const GptSearchBar = () => {
   const dispatch = useDispatch();
   const langKey = useSelector((store) => store.config.lang);
   const searchText = useRef(null);
+  const notify = () => toast.error("Sorry for the inconvenience. openAI API calls seem to have reached their limit.");
+
 
   // search movie in TMDB
   const searchMovieTMDB = async (movie) => {
@@ -24,43 +28,48 @@ const GptSearchBar = () => {
   };
 
   const handleGptSearchClick = async () => {
-    console.log(searchText.current.value);
-    // Make an API call to GPT API and get Movie Results
-
-    //==========================================================
-    const gptQuery =
-      "Act as a Movie Recommendation system and suggest some movies for the query : " +
-      searchText.current.value +
-      ". only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
-
-    const gptResults = await openai.chat.completions.create({
-      messages: [{ role: "user", content: gptQuery }],
-      model: "gpt-3.5-turbo",
-    });
-
-    if (!gptResults.choices) {
-      // TODO: Write Error Handling
+    try{
+      console.log(searchText.current.value);
+      // Make an API call to GPT API and get Movie Results
+  
+      //==========================================================
+      const gptQuery =
+        "Act as a Movie Recommendation system and suggest some movies for the query : " +
+        searchText.current.value +
+        ". only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
+  
+      const gptResults = await openai.chat.completions.create({
+        messages: [{ role: "user", content: gptQuery }],
+        model: "gpt-3.5-turbo",
+      });
+  
+      console.log(gptResults.choices?.[0]?.message?.content);
+  
+      // Andaz Apna Apna, Hera Pheri, Chupke Chupke, Jaane Bhi Do Yaaro, Padosan
+      const gptMovies = gptResults.choices?.[0]?.message?.content.split(",");
+  
+      // ["Andaz Apna Apna", "Hera Pheri", "Chupke Chupke", "Jaane Bhi Do Yaaro", "Padosan"]
+  
+      //============================================================
+  
+      // For each movie I will search TMDB API
+      const promiseArray = gptMovies.map((movie) => searchMovieTMDB(movie));
+      
+      const tmdbResults = await Promise.all(promiseArray);
+  
+      console.log(tmdbResults);
+  
+      dispatch(
+        addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults })
+      );
     }
-
-    console.log(gptResults.choices?.[0]?.message?.content);
-
-    // Andaz Apna Apna, Hera Pheri, Chupke Chupke, Jaane Bhi Do Yaaro, Padosan
-    const gptMovies = gptResults.choices?.[0]?.message?.content.split(",");
-
-    // ["Andaz Apna Apna", "Hera Pheri", "Chupke Chupke", "Jaane Bhi Do Yaaro", "Padosan"]
-
-    //============================================================
-
-    // For each movie I will search TMDB API
-    const promiseArray = gptMovies.map((movie) => searchMovieTMDB(movie));
+    catch{
+      
+        // TODO: Write Error Handling
+        notify();
     
-    const tmdbResults = await Promise.all(promiseArray);
-
-    console.log(tmdbResults);
-
-    dispatch(
-      addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults })
-    );
+    }
+   
   };
 
   return (
@@ -81,6 +90,18 @@ const GptSearchBar = () => {
         >
           {lang[langKey].search}
         </button>
+        <ToastContainer
+              position="top-center"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            ></ToastContainer>
       </form>
     </div>
   );
